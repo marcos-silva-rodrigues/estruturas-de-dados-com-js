cd35d2e2310c9344e5aef6dc6acd152abd6eea52
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-01-27 22:47:23 -0300

    feat: finalizando cap 5, Filas e Deques

---
+++ 05-filas/deque.js
@@ -0,0 +1,147 @@
+class Deque {
+  constructor() {
+    this.count = 0;
+    this.lowestCount = 0;
+    this.items = {};
+  }
+
+  addFront(element) {
+    if (this.isEmpty()) {
+      this.addBack(element);
+    } else if (this.lowestCount > 0) {
+      this.lowestCount--;
+      this.items[this.lowestCount] = element;
+    } else {
+      for (let i = this.count; i > 0; i--) {
+        this.items[i] = this.items[i - 1];
+      }
+
+      this.count++;
+      this.lowestCount = 0;
+      this.items[0] = element;
+    }
+  }
+
+  addBack(element) {
+    this.items[this.count] = element;
+    this.count++;
+  }
+
+  removeFront() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+
+    const result = this.items[this.lowestCount];
+    delete this.items[this.lowestCount];
+    this.lowestCount++;
+    return result;
+  }
+
+  removeBack() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+
+    this.count--;
+    const result = this.items[this.count];
+    delete this.items[this.count];
+    return result;
+  }
+
+  peekFront() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+
+    return this.items[this.lowestCount];
+  }
+
+  peekBack() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.items[this.count - 1];
+  }
+
+  isEmpty() {
+    return this.count - this.lowestCount === 0;
+  }
+
+  size() {
+    return this.count - this.lowestCount;
+  }
+
+  clear() {
+    this.items = {};
+    this.count = 0;
+    this.lowestCount = 0;
+  }
+
+  toString() {
+    if (this.isEmpty()) {
+      return "";
+    }
+    let objString = `${this.items[this.lowestCount]}`;
+    for (let i = this.lowestCount + 1; i < this.count; i++) {
+      objString = `${objString}, ${this.items[i]}`;
+    }
+
+    return objString;
+  }
+}
+
+const deque = new Deque();
+
+console.log(deque.isEmpty());
+
+deque.addBack("John");
+deque.addBack("Jack");
+
+console.log(deque.toString());
+
+deque.addBack("Camila");
+console.log(deque.size());
+
+deque.removeFront();
+console.log(deque.toString());
+
+deque.removeBack();
+console.log(deque.toString());
+deque.addFront("John");
+console.log(deque);
+
+function palindromoChecker(aString) {
+  if (
+    aString === undefined ||
+    aString === null ||
+    (aString !== null && aString.length === 0)
+  ) {
+    return false;
+  }
+
+  const deque = new Deque();
+  const lowerString = aString.toLocaleLowerCase().split(' ').join('');
+  let isEqual = true;
+  let firstChar, lastChar;
+
+  for (let i = 0; i < lowerString; i++) {
+    deque.addBack(lowerString.charAt(i));
+  }
+
+  while(deque.size() > 1 && isEqual) {
+    firstChar = deque.removeFront();
+    lastChar = deque.removeBack();
+
+    if (firstChar !== lastChar) {
+      isEqual = false;
+    }
+  }
+
+  return isEqual;
+}
+
+console.log('a', palindromoChecker('a'));
+console.log('level', palindromoChecker('level'));
+console.log('Was it a car ou a cat I saw', palindromoChecker('Was it a car ou a cat I saw'));
+console.log('Step on no pets', palindromoChecker('Step on no pets'));
---
+++ 05-filas/index.js
@@ -0,0 +1,101 @@
+class Queue {
+  constructor() {
+    this.count = 0;
+    this.lowestCount = 0;
+    this.items = {};
+  }
+
+  enqueue(element) {
+    this.items[this.count] = element;
+    this.count++;
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+
+    const result = this.items[this.lowestCount];
+    delete this.items[this.lowestCount];
+    this.lowestCount++;
+    return result;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+
+    return this.items[this.lowestCount];
+  }
+
+  isEmpty() {
+    return this.count - this.lowestCount === 0;
+  }
+
+  size() {
+    return this.count - this.lowestCount;
+  }
+
+  clear() {
+    this.items = {};
+    this.count = 0;
+    this.lowestCount = 0;
+  }
+
+  toString() {
+    if(this.isEmpty()) {
+      return '';
+    }
+    let objString = `${this.items[this.lowestCount]}`;
+    for (let i = this.lowestCount + 1; i < this.count; i++) {
+      objString = `${objString}, ${this.items[i]}`;
+    }
+
+    return objString;
+  }
+}
+
+const queue = new Queue();
+
+console.log(queue.isEmpty());
+
+queue.enqueue('Teste');
+queue.enqueue('Marcos');
+console.log(queue.toString())
+
+console.log(queue.size());
+
+queue.enqueue('maria');
+console.log(queue.isEmpty());
+
+queue.dequeue();
+queue.dequeue();
+
+console.log(queue.peek());
+
+function hotPotato(elementsList, num) {
+  const queue = new Queue();
+  const elimitatedList = [];
+
+  for (let i = 0; i < elementsList.length; i++) {
+    queue.enqueue(elementsList[i]);
+  }
+
+  while (queue.size() > 1) {
+    for (let i = 0; i < num; i++) {
+      queue.enqueue(queue.dequeue());
+    }
+    elimitatedList.push(queue.dequeue());
+  }
+
+  return {
+    eliminated: elimitatedList,
+    winner: queue.dequeue()
+  }
+}
+
+const names = ['John', 'Jack', 'Camila', 'Ingrid','Carl'];
+const result = hotPotato(names, 7);
+result.eliminated.forEach(name => console.log(`${name} was eliminated from the Hot Potato game.`));
+console.log(`The winner is: ${result.winner}`);
