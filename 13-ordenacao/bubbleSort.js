bf60194b1269c6d860cb0b603d813d698cc9b5d5
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-06-11 13:06:47 -0300

    feat: bubble sort

---
+++ 13-ordenacao/bubbleSort.js
@@ -0,0 +1,29 @@
+import { Compare, defaultCompare, swap } from '../utils/index.js';
+
+export function bubbleSort(array, compareFn = defaultCompare) {
+  const { length } = array;
+
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length - 1; j++) {
+      if (compareFn(array[j], array[j + 1]) === Compare.BIGGER_THAN) {
+        swap(array, j, j+1);
+      }
+    }
+  }
+
+  return array;
+}
+
+export function modifiedBubbleSort(array, compareFn = defaultCompare) {
+  const { length } = array;
+
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length - 1 - i; j++) {
+      if (compareFn(array[j], array[j + 1]) === Compare.BIGGER_THAN) {
+        swap(array, j, j+1);
+      }
+    }
+  }
+
+  return array;
+}
---
+++ 13-ordenacao/index.js
@@ -0,0 +1,21 @@
+import { bubbleSort, modifiedBubbleSort } from "./bubbleSort.js";
+
+function createNonSortedArray(size) {
+  const array = [];
+  for (let i = size; i > 0; i--) {
+    array.push(i);
+  }
+
+  return array;
+}
+
+let array = createNonSortedArray(5);
+console.log(array);
+
+// Bubble Sort
+// array = bubbleSort(array);
+// console.log(array);
+
+// Modified Bubble Sort
+array = modifiedBubbleSort(array);
+console.log(array);
