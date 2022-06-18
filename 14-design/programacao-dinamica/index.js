581f6a580fec01bfd89e9190dd7d3b6343183512
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-06-18 14:15:17 -0300

    feat: problema da mochila

--- 14-design/programacao-dinamica/index.js
+++ 14-design/programacao-dinamica/index.js
@@ -1,3 +1,11 @@
+import { knapSack } from "./knapSack.js";
 import { minCoinChange } from "./minCoinChange.js";
 
-console.log(minCoinChange([1, 5, 10, 25], 36));
+// console.log(minCoinChange([1, 5, 10, 25], 36));
+
+const values = [3, 4, 5],
+  weights = [2, 3, 4],
+  capacity = 5,
+  n = values.length;
+
+console.log(knapSack(capacity, weights, values, n));
