472df37b7cd04c5c0a980fdd9b2627ea2b99573c
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-06-18 19:30:26 -0300

    feat: knap snack version 2

--- 14-design/algoritmos-gulosos/index.js
+++ 14-design/algoritmos-gulosos/index.js
@@ -1,3 +1,6 @@
+import { knapSnack } from "./knapSnack.js";
 import { minCoinChange } from "./minCoinChange.js";
 
-console.log(minCoinChange([1, 5, 10, 25], 36));
+// console.log(minCoinChange([1, 5, 10, 25], 36));
+
+console.log(knapSnack(6, [2, 3, 4], [3, 4, 5]));
